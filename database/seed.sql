0a7ec20bf0000c38a85017ce81034910fac990be
Bryan H Cruz <bryanhcruz@outlook.com>
2024-12-09 22:38:10 -0300

    atualzia taxas de cambiio

--- database/seed.sql
+++ database/seed.sql
@@ -1,7 +1,8 @@
 -- Inserir moedas
 INSERT INTO currencies (name, code, symbol,rate) VALUES
     ('Dólar Americano', 'USD', '$', 1),
-    ('Euro', 'EUR', '€', 0.95),
-    ('Real Brasileiro', 'BRL', 'R$', 5.81),
-    ('Iene Japonês', 'JPY', '¥', 152.62),
-    ('Peso Argentino', 'ARS', '$', 1007.23);
+    ('Euro', 'EUR', '€', 0.948),
+    ('Real Brasileiro', 'BRL', 'R$', 6.07),
+    ('Iene Japonês', 'JPY', '¥', 151.38),
+    ('Peso Argentino', 'ARS', '$', 1015.74);
+    ('Bitcoin', 'BTC', '₿', 0.000010);
